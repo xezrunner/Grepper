15496de7ede790876d34684bf055d70031155ba7
xezrunner <8061077+xezrunner@users.noreply.github.com>
2025-08-27 12:45:56 +0200

    Initial Commit

---
+++ Grepper/ContentView.swift
@@ -0,0 +1,13 @@
+// Grepper::ContentView.swift - 27/08/2025
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Grepper/GrepperApp.swift
@@ -0,0 +1,12 @@
+// Grepper::GrepperApp.swift - 27/08/2025
+
+import SwiftUI
+
+@main
+struct GrepperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
